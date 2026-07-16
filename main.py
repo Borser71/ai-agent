63b52a82c3d4ae02a5dfc1ec42855aa6f9b3b23d
Сергей <wisebor@gmail.com>
2026-07-16 19:29:30 +0500

    Update main.py
    inter

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ app.add_middleware(
         "https://www.borisov.store",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://ai-agent-bnei.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
